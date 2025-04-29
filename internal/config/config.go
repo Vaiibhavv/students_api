3f74a144ae999e233ca092d2d21b62a0348f66aa
Vaibhav Patil <107416335+Vaiibhavv@users.noreply.github.com>
2025-04-29 08:22:44 +0000

    gracefull shutdonw

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,38 +8,35 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type HTTPServer struct
-{
-	Address string
+type HTTPServer struct {
+	Address string `yaml:"address" env-required:"true"`
 }
 
-type Config struct{
-	Env string  `yaml:"env" env:"ENV" env-required:"true"`
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
-
+	HTTPServer  `yaml:"http_server"`
 }
 
 func MustLoad() *Config {
 	var ConfigPath string
 
-	ConfigPath=os.Getenv("")
-	if ConfigPath==""{
-		flags:=flag.String("config","","path related to config path")
+	ConfigPath = os.Getenv("")
+	if ConfigPath == "" {
+		flags := flag.String("config", "", "path related to config path")
 		flag.Parse()
 
-		ConfigPath=*flags
+		ConfigPath = *flags
 
-		if ConfigPath==""{
+		if ConfigPath == "" {
 			log.Fatal("config path is not set")
 		}
-
 	}
 
 	// check on provided path we have given the config file or not
 
-	if _,err:=os.Stat(ConfigPath);os.IsNotExist(err){
-		log.Fatalf("config path file does not exist %s",ConfigPath)
+	if _, err := os.Stat(ConfigPath); os.IsNotExist(err) {
+		log.Fatalf("config path file does not exist %s", ConfigPath)
 	}
 
 	// if file set, we have to read the file and serialze the file as per the enviroment (eg, local,prod)
@@ -47,15 +44,13 @@ func MustLoad() *Config {
 	var cfg Config
 
 	//cleanenv.Readconfig  - return the error
-	
-	err:=cleanenv.ReadConfig(ConfigPath,&cfg)
-	if err!=nil{
-		log.Fatalf("error while reading the configpath file %s",err.Error())
+
+	err := cleanenv.ReadConfig(ConfigPath, &cfg)
+	if err != nil {
+		log.Fatalf("error while reading the configpath file %s", err.Error())
 
 	}
 
 	return &cfg
-	
-	
 
 }
